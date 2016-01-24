6ca92e1fbde3fcd2563f4318c737b6d666ca3930
Ranjan Anantharaman <ranjan@juliacomputing.com>
2016-01-24 08:28:47 +0000

    Small fix : connected components

--- src/cc.jl
+++ src/cc.jl
@@ -1,4 +1,4 @@
-function cc(G::SparseMatrixCSC, root::Int)
+function cc(G::SparseMatrixCSC)
 	rows = G.colptr - 1
     cols = G.rowval - 1
     nodes = length(rows) - 1
@@ -6,7 +6,7 @@ function cc(G::SparseMatrixCSC, root::Int)
     rows = map(Int32, rows)
     cols = map(Int32, cols)
     conn_comp = Array(Int32, nodes)
-	num_cc = ccall((:bfs, path_to_gunrock), Cint, (Ptr{Cint}, Int, Int, Ptr{Cint}, Ptr{Cint}, Int), conn_comp, nodes, edges, rows, cols, root - 1)
+	num_cc = ccall((:cc, path_to_gunrock), Cint, (Ptr{Cint}, Int, Int, Ptr{Cint}, Ptr{Cint}), conn_comp, nodes, edges, rows, cols)
     conn_comp += 1
 	num_cc, conn_comp
 end
--- src/consts.jl
+++ src/consts.jl
@@ -1,1 +1,1 @@
-const path_to_gunrock = ""
+const path_to_gunrock = "/home/ubuntu/gunrock2/build/lib/libgunrock.so"
